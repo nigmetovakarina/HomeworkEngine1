7a6a1a49f08cbe91e18c853b854b87a538f99db7
nigmetovakarina <nigmetova.karina@gmail.com>
2025-11-20 17:00:15 +0500

    update update

--- Assets/scripts/Character/components/Movement/DefaultMovementComponent.cs
+++ Assets/scripts/Character/components/Movement/DefaultMovementComponent.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 public class DefaultMovementComponent : MonoBehaviour
 {
     public float Speed { get; set; }
+    public float speed;
 
     public Vector3 Position { get; }
 
+    public void Initialize(CharacterData characterData)
+    {
+        this.characterData = characterData;
+        speed = characterData.DefaultSpeed;
+    }
 
     public void Move(Vector3 direction) { }
 
